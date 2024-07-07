a479f643b00c2fece2f7f61b7b7b859f6f1c2c8e
katrinawoo <katrinawoo@gmail.com>
2024-07-07 07:07:31 -0400

    Created a component for the logo element on the bottom left corner of every page in tablet view.

---
+++ src/components/FooterLogo/FooterLogo.jsx
@@ -0,0 +1,14 @@
+import logoBird from "../../assets/logo/logo-bird.png";
+import "./FooterLogo.scss";
+
+const FooterLogo = () => {
+  return (
+    <div className="footer-logo">
+      <div className="footer-logo__line"></div>
+      <div className="footer-logo__text">PATRIOTS DIVISION</div>
+      <img src={logoBird} alt="Patriots Division Logo" className="footer-logo__logo" />
+    </div>
+  );
+};
+
+export default FooterLogo;
