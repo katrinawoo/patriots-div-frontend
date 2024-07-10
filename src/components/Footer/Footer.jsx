8dea61164a3f52a65882a5a5a6b7f9b9d2cf647c
katrinawoo <katrinawoo@gmail.com>
2024-07-09 22:57:17 -0400

    Footer styling for mobile.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,121 +14,63 @@ export default function Footer() {
     <footer className="footer">
       <div className="footer__wrapper">
         <article className="footer__content">
-            <div className="footer__links footer__links--1">
-                <h3>Games</h3>
-                <div>
+            <div className="footer__links footer__links__column">
+                <div className="footer__links__title">Games</div>
+                <div className="footer__links__title--sites">
                     <Link to="/">
                         Vision
                     </Link>
                 </div>
-                <div>
+                <div className="footer__links__title--sites">
                     <Link to="https:www.shadowwar.com">
                         Shadow War
                     </Link>
                 </div>
             </div>
 
-            <div className="footer__links footer__links--2">
-                <h3>About</h3>
-                <div>
+            <div className="footer__links footer__links__column">
+                <div className="footer__links__title">About</div>
+                <div className="footer__links__title--sites">
                     <Link to="/team">
                         Work with us
                     </Link>
                 </div>
-                <div>
+                <div className="footer__links__title--sites">
                     <Link to="/careers">
                         Jobs
                     </Link>
                 </div>
             </div>
 
-            {/* <section className="games-home-page__actions">
-                <Link className="games-home-page__actions__button" to="/careers">
-                  Check Open Positions <img src={arrowIcon} alt="Right Arrow" className="games-home-page__actions__icon" />
-                </Link>
-                <div></div>
-            </section> */}
-
         </article>
+
+        <div className="footer__logo">
+                <Link to="/">
+                    <img className="footer__logo" src={logo} alt="Patriots Division logo" />
+                </Link>
+        </div>
 
         <article className="footer__media">
             <div className="footer__media--socials">
                 <Link to="https://twitter.com/PatriotsPMC">
-                    <img className="footer__logo__icon" src={twitterIcon} alt="Twitter Icon" />
+                    <img className="footer__media__icon" src={twitterIcon} alt="Twitter Icon" />
                 </Link>
             </div>
 
             <div className="footer__media--socials">
                 <Link to="https://www.linkedin.com/company/patriots-division/">
-                    <img className="footer__logo__icon" src={linkedinIcon} alt="LinkedIn Icon" />
+                    <img className="footer__media__icon" src={linkedinIcon} alt="LinkedIn Icon" />
                 </Link>
             </div>
+        </article>
 
-            <div className="footer__media--name">
+        <div className="footer__media--name">
                 <p>Patriots Division - 2024</p>
             </div>
 
-            <div className="footer__media--tagline">
-                <p>Break - Surveillance - 20XX</p>
-            </div>
-        </article>
-        {/* <div className="footer__logo">
-          <Link to="/">
-            <img className="footer__logo__img" src={logo} alt="patriots division logo" />
-          </Link>
+        <div className="footer__media--tagline">
+            <p>Break - Surveillance - 20XX</p>
         </div>
-        
-        <nav className="footer__nav">
-          <div className="footer__nav--tablet">
-            <NavLink
-                className={`footer__nav__item ${
-                isHomeActive ? "active" : ""
-                }`}
-                to="/"
-            >
-                GAMES
-            </NavLink>
-            
-            <NavLink
-                className={`footer__nav__item ${
-                location.pathname.includes("team") ? "active" : ""
-                }`}
-                to="/team"
-            >
-                TEAM
-            </NavLink>
-
-            <NavLink
-                className={`footer__nav__item ${
-                location.pathname.includes("careers") ? "active" : ""
-                }`}
-                to="/careers"
-            >
-                CAREERS
-            </NavLink>
-
-            <NavLink
-                className={`footer__nav__item ${
-                location.pathname.includes("contacts") ? "active" : ""
-                }`}
-                to="/contacts"
-            >
-                CONTACTS
-            </NavLink>
-          </div>
-          
-          <div className="footer__nav--mobile">
-            <img
-              className="footer__nav__menu-icon"
-              src={menuIcon}
-              alt="menu icon"
-              onClick={toggleModal}
-            />
-          </div>
-        </nav>
-
-        <div className="footer__nav--tablet footer__nav--tablet--empty">
-        </div> */}
 
       </div>
 
