5f2b717e8298ebd1d53c48b0494cd7c121e38014
katrinawoo <katrinawoo@gmail.com>
2024-07-09 23:38:28 -0400

    Footer styling for desktop.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,13 +6,48 @@ import linkedinIcon from "../../assets/icons/icons8-linkedin-24.png";
 import twitterIcon from "../../assets/icons/icons8-twitter-24.png";
 // import menuIcon from "../../assets/icons/1193-nav-menu-icon.png";
 import "./Footer.scss";
-import { ssrImportKey } from "vite/runtime";
 
 export default function Footer() {
   
   return (
     <footer className="footer">
       <div className="footer__wrapper">
+        
+
+        <div className="footer__logo footer__logo--tablet">
+                <Link to="/">
+                    <img className="footer__logo" src={logo} alt="Patriots Division logo" />
+                </Link>
+        </div>
+
+        
+        <div className="footer__wrapper--tablet">
+        <div className="footer__media--box">
+                <div className="footer__media--name footer__media--tablet">
+                        <p>Patriots Division - 2024</p>
+                    </div>
+
+                <div className="footer__media--tagline footer__media--tablet">
+                    <p>Break - Surveillance - 20XX</p>
+                </div>
+            </div>
+
+            <article className="footer__media footer__media--tablet">
+                <div className="footer__media--socials">
+                    <a href="https://twitter.com/PatriotsPMC" target="_blank" rel="noopener" title="Visit Twitter">
+                        <img src={twitterIcon} alt="twitter" className="footer__media__icon"/>
+                    </a>
+                </div>
+
+                <div className="footer__media--socials">
+                    <a href="https://www.linkedin.com/company/patriots-division/" target="_blank" rel="noopener" title="Visit LinkedIn">
+                        <img src={linkedinIcon} alt="linkedIn" className="footer__media__icon"/>
+                    </a>
+                </div>
+            </article>
+        </div>
+
+
         <article className="footer__content">
             <div className="footer__links footer__links__column">
                 <div className="footer__links__title">Games</div>
@@ -44,34 +79,31 @@ export default function Footer() {
 
         </article>
 
-        <div className="footer__logo">
+        <div className="footer__logo footer__logo--mobile">
                 <Link to="/">
                     <img className="footer__logo" src={logo} alt="Patriots Division logo" />
                 </Link>
         </div>
 
-        <article className="footer__media">
+        <article className="footer__media footer__media--mobile">
             <div className="footer__media--socials">
                 <a href="https://twitter.com/PatriotsPMC" target="_blank" rel="noopener" title="Visit Twitter">
-                    <img src={twitterIcon} alt="twitter" class="footer__media__icon"/>
+                    <img src={twitterIcon} alt="twitter" className="footer__media__icon"/>
                 </a>
             </div>
 
             <div className="footer__media--socials">
-                {/* <Link to="https://www.linkedin.com/company/patriots-division/">
-                    <img className="footer__media__icon" src={linkedinIcon} alt="LinkedIn Icon" />
-                </Link> */}
                 <a href="https://www.linkedin.com/company/patriots-division/" target="_blank" rel="noopener" title="Visit LinkedIn">
-                    <img src={linkedinIcon} alt="linkedIn" class="footer__media__icon"/>
+                    <img src={linkedinIcon} alt="linkedIn" className="footer__media__icon"/>
                 </a>
             </div>
         </article>
 
-        <div className="footer__media--name">
+        <div className="footer__media--name footer__media--mobile">
                 <p>Patriots Division - 2024</p>
             </div>
 
-        <div className="footer__media--tagline">
+        <div className="footer__media--tagline footer__media--mobile">
             <p>Break - Surveillance - 20XX</p>
         </div>
 
