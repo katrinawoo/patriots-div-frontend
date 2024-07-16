7865ca15b9010079cf14292afa296fa276977d75
katrinawoo <katrinawoo@gmail.com>
2024-07-16 04:07:52 -0400

    Resume upload button styling.

--- src/components/JobApplicationForm/JobApplicationForm.jsx
+++ src/components/JobApplicationForm/JobApplicationForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react'; // Added useRef import
 import { apply } from '../../utils/api';
 import arrowIcon from "../../assets/icons/Vector-button-arrow.png";
 import jobApplicationValidator from '../../utils/jobApplicationValidator';
@@ -15,16 +15,21 @@ const JobApplicationForm = ({ id }) => {
   const [information, setInformation] = useState('');
   const [informationLength, setInformationLength] = useState(MAX_INFORMATION_LENGTH);
   const [resume, setResume] = useState(null);
+  const [resumeName, setResumeName] = useState(''); // Added state for resume name
   const [errors, setErrors] = useState({});
   const [successMessage, setSuccessMessage] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const [showPopup, setShowPopup] = useState(false);
 
+  const fileInputRef = useRef(null); // Added file input ref
+
   const handleResumeChange = (e) => {
-    setResume(e.target.files[0]);
+    const file = e.target.files[0];
+    setResume(file);
+    setResumeName(file ? file.name : ''); // Set resume name
   };
 
-const handleInformationChange = (e) => {
+  const handleInformationChange = (e) => {
     const { value } = e.target;
     if (value.length <= MAX_INFORMATION_LENGTH) {
       setInformation(value);
@@ -49,11 +54,6 @@ const handleInformationChange = (e) => {
     formData.append('information', information);
     formData.append('resume', resume);
 
-// Log FormData to verify its contents
-for (let pair of formData.entries()) {
-    console.log(pair[0] + ': ' + pair[1]);
-  }
-
     try {
       await apply(id, formData);
       setSuccessMessage('Thanks Patriot, your application has been submitted!');
@@ -66,94 +66,111 @@ for (let pair of formData.entries()) {
       setLinks('');
       setInformation('');
       setResume(null);
+      setResumeName(''); // Reset resume name
     } catch (error) {
       setErrorMessage('Failed mission to send application. Please try again.');
       setSuccessMessage('');
     }
   };
 
+  const handleFileButtonClick = () => {
+    fileInputRef.current.click(); // Trigger file input click
+  };
+
   return (
     <div className="job-application-form__wrapper">
-    <form className="job-application-form" onSubmit={handleSubmit}>
-      <div className={`job-application-form__group ${errors.name ? 'error' : ''}`}>
-        <label htmlFor="name"><h2>Name:</h2></label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Your Name"
-        />
-        {errors.name && <p className="job-application-form__error-message">{errors.name}</p>}
-      </div>
-      <div className={`job-application-form__group ${errors.email ? 'error' : ''}`}>
-        <label htmlFor="email"><h2>Email:</h2></label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="your.email@example.com"
-        />
-        {errors.email && <p className="job-application-form__error-message">{errors.email}</p>}
-      </div>
-      <div className={`job-application-form__group ${errors.linkedin ? 'error' : ''}`}>
-        <label htmlFor="linkedin"><h2>LinkedIn:</h2></label>
-        <input
-          type="text"
-          id="linkedin"
-          name="linkedin"
-          value={linkedin}
-          onChange={(e) => setLinkedin(e.target.value)}
-          placeholder="LinkedIn Profile URL"
-        />
-        {errors.linkedin && <p className="job-application-form__error-message">{errors.linkedin}</p>}
-      </div>
-      <div className={`job-application-form__group ${errors.links ? 'error' : ''}`}>
-        <label htmlFor="links"><h2>Other Links:</h2></label>
-        <input
-          type="text"
-          id="links"
-          name="links"
-          value={links}
-          onChange={(e) => setLinks(e.target.value)}
-          placeholder="Portfolio Links, Twitter Handle, etc.  Help us get to know you better!"
-        />
-        {errors.links && <p className="job-application-form__error-message">{errors.links}</p>}
-      </div>
-      <div className={`job-application-form__group ${errors.information ? 'error' : ''}`}>
-        <label htmlFor="information"><h2>Information:</h2></label>
-        <textarea
-          id="information"
-          name="information"
-          value={information}
-          onChange={handleInformationChange}
-          placeholder="Let us know why you think this role may be a good fit for you, or how your previous experience relates!"
-        />
-        {errors.information && <p className="job-application-form__error-message">{errors.information}</p>}
-        <p className="job-application-form__character-count">{informationLength} characters left</p>
-      </div>
-      <div className={`job-application-form__group ${errors.resume ? 'error' : ''}`}>
-        <label htmlFor="resume"><h2>Resume:</h2><div>Format of .pdf only, maximum size 5 MB.</div></label>
-        <input
-          type="file"
-          id="resume"
-          name="resume"
-          accept=".pdf"
-          onChange={handleResumeChange}
-        />
-        {errors.resume && <p className="job-application-form__error-message">{errors.resume}</p>}
-      </div>
-      <button type="submit" className="job-application-form__submit__button">
-        Submit <img src={arrowIcon} alt="Right Arrow" className="job-application-form__submit__icon" />
-      </button>
-      {successMessage && <SuccessPopup show={showPopup} title={successMessage} text="" />}
-      {errorMessage && <p className="job-application-form__error-message">{errorMessage}</p>}
-    </form>
-  </div>
-);
+      <form className="job-application-form" onSubmit={handleSubmit}>
+        <div className={`job-application-form__group ${errors.name ? 'error' : ''}`}>
+          <label htmlFor="name"><h2>Name:</h2></label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Your Name"
+          />
+          {errors.name && <p className="job-application-form__error-message">{errors.name}</p>}
+        </div>
+        <div className={`job-application-form__group ${errors.email ? 'error' : ''}`}>
+          <label htmlFor="email"><h2>Email:</h2></label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="your.email@example.com"
+          />
+          {errors.email && <p className="job-application-form__error-message">{errors.email}</p>}
+        </div>
+        <div className={`job-application-form__group ${errors.linkedin ? 'error' : ''}`}>
+          <label htmlFor="linkedin"><h2>LinkedIn:</h2></label>
+          <input
+            type="text"
+            id="linkedin"
+            name="linkedin"
+            value={linkedin}
+            onChange={(e) => setLinkedin(e.target.value)}
+            placeholder="LinkedIn Profile URL"
+          />
+          {errors.linkedin && <p className="job-application-form__error-message">{errors.linkedin}</p>}
+        </div>
+        <div className={`job-application-form__group ${errors.links ? 'error' : ''}`}>
+          <label htmlFor="links"><h2>Other Links:</h2></label>
+          <input
+            type="text"
+            id="links"
+            name="links"
+            value={links}
+            onChange={(e) => setLinks(e.target.value)}
+            placeholder="Portfolio Links, Twitter Handle, etc.  Help us get to know you better!"
+          />
+          {errors.links && <p className="job-application-form__error-message">{errors.links}</p>}
+        </div>
+        <div className={`job-application-form__group ${errors.information ? 'error' : ''}`}>
+          <label htmlFor="information"><h2>Information:</h2></label>
+          <textarea
+            id="information"
+            name="information"
+            value={information}
+            onChange={handleInformationChange}
+            placeholder="Let us know why you think this role may be a good fit for you, or how your previous experience relates!"
+          />
+          {errors.information && <p className="job-application-form__error-message">{errors.information}</p>}
+          <p className="job-application-form__character-count">{informationLength} characters left</p>
+        </div>
+        <div className={`job-application-form__group ${errors.resume ? 'error' : ''}`}>
+          <label htmlFor="resume">
+            <h2>Resume:</h2>
+            <div>Format of .pdf only, maximum size 5 MB.</div>
+          </label>
+          <input
+            type="file"
+            id="resume"
+            name="resume"
+            accept=".pdf"
+            onChange={handleResumeChange}
+            ref={fileInputRef} // Added ref to file input
+            style={{ display: 'none' }}
+          />
+          <div job-application-form__file-box>
+            <div onClick={handleFileButtonClick} className="job-application-form__file-button">
+                Choose File
+            </div>
+            {resumeName && <span className="job-application-form__file-name">{resumeName}</span>}
+            {errors.resume && <p className="job-application-form__error-message">{errors.resume}</p>}
+          </div>
+        </div>
+        <button type="submit" className="job-application-form__submit__button">
+          Submit <img src={arrowIcon} alt="Right Arrow" className="job-application-form__submit__icon" />
+        </button>
+        {successMessage && <SuccessPopup show={showPopup} title={successMessage} text="" />}
+        {errorMessage && <p className="job-application-form__error-message">{errorMessage}</p>}
+      </form>
+    </div>
+  );
 };
 
 export default JobApplicationForm;
+
